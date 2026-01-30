350773cb1d12c580ec64b94da4f951bafac599dd
Lydia Allen <lydia.arakelian.allen@gmail.com>
2026-01-30 10:18:53 -0800

    compute pay function does not take into account negative numbers

--- sum_of_two_ints.py
+++
@@ -1,9 +0,0 @@
-# written 1/20/26 by Lydia Allen
-# gets the first integer from the user and converts it from a string to an integer
-first = input("Enter first number: ")
-first = int(first)
-# gets the second integer from the user and convers from a string to an integer
-second = input("Enter second number: ")
-second = int(second)
-# adds the two integers
-print(first + second)
